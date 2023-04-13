5bc02c5da6cc415fd87c715269b8bf16b81f4dc8
Jonas Holbech <jh@jonasholbech.dk>
2023-04-13 10:54:35 +0200

    mapping

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,33 @@ function App() {
     age: 18,
     email: "bo@bo.dk",
   };
+  const companies = [
+    { name: "Pentia", description: "Consultant House", employees: 100 },
+    { name: "Signifly", description: "Digital Agency", employees: 45 },
+  ];
+
   return (
     <div className="App">
       <Navigation />
+
+      <section className="brands">
+        <ul>
+          {companies.map((company) => (
+            <Company name={company.name} description={company.description} />
+          ))}
+        </ul>
+      </section>
       <ValueGrid user={user} />
     </div>
   );
 }
 
+function Company(props) {
+  return (
+    <li>
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
 export default App;
